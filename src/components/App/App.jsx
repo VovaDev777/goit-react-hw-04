8b9727f55fe005c50ea3208b1880a1aaae693c29
VovaDev777 <polvovater@gmail.com>
2024-07-11 00:22:28 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import '../../reset.css'
 import ImageGallery from '../ImageGallery/ImageGallery'
 
-const ACCES_KEY = "YGRmWj6GDedxJ_oXB0FmTQ150ytZjLND_WU28-vdnDY"
+
 
 
 const App = () => {
@@ -12,12 +12,7 @@ const App = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        async function fetchPhotos(query) {
-            const response = await axios.get(
-                `https://api.unsplash.com/photos/?client_id=${ACCES_KEY}&query=${query}`
-            );
-            setData(response.data);
-        }
+        
         // getData(searchWord);
         // console.log(data)
         
--- src/fetchData.js
+++ src/fetchData.js
@@ -1,1 +1,10 @@
 import axios from 'axios'
+
+const ACCES_KEY = "YGRmWj6GDedxJ_oXB0FmTQ150ytZjLND_WU28-vdnDY"
+
+export const fetchPhotos = async (query) => {
+    const response = await axios.get(
+        `https://api.unsplash.com/photos/?client_id=${ACCES_KEY}&query=${query}`
+    );
+    return response.data;
+}
