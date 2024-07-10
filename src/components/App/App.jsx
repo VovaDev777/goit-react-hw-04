49d4b7051ede3688d22d7140140e08e48a8509a3
VovaDev777 <polvovater@gmail.com>
2024-07-10 15:01:14 +0200

    1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import Header from '../Header/Header'
+import { useEffect, useState } from 'react'
+
+
+const App = () => {
+
+
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default App
