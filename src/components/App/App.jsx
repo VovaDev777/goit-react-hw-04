91f95830ae2dc61f6f4eb228592dddca9056760e
VovaDev777 <polvovater@gmail.com>
2024-07-10 23:20:51 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,26 +8,25 @@ const ACCES_KEY = "YGRmWj6GDedxJ_oXB0FmTQ150ytZjLND_WU28-vdnDY"
 
 
 const App = () => {
+
     const [data, setData] = useState([]);
+
     useEffect(() => {
         async function getData() {
             const response = await axios.get(
                 `https://api.unsplash.com/photos/?client_id=${ACCES_KEY}&query=car`
             );
-            console.log(response.data);
+            setData(response.data);
         }
 
         getData();
     }, [])
 
-
     const getSearchValue = (value) => {
         console.log(value)
     }
 
-
-
-
+    console.log(data)
     return (
         <>
             <Header onAdd={getSearchValue} />
