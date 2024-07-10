42d837aa70391d502725361c01e8318564db6fcf
VovaDev777 <polvovater@gmail.com>
2024-07-10 15:23:06 +0200

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,16 @@
 // import React from 'react'
 import Header from '../Header/Header'
 import { useEffect, useState } from 'react'
+import '../../reset.css'
 
 
 const App = () => {
 
 
   return (
-    <div>
+    <>
         <Header />
-    </div>
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
-
+import css from './Header.module.css'
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Header = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <header className={css.d1}>
+            <form>
+                <input
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+                
+                <button type="submit">
+                <AiOutlineSearch />
+                </button>
+            </form>
+        </header>
+
+    )
 }
 
 export default Header
