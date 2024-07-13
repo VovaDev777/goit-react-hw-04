03c3e59611538e2681df21c0aecd48f9d7a06688
VovaDev777 <polvovater@gmail.com>
2024-07-13 22:32:29 +0200

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -16,6 +16,18 @@ const Header = ({ onAdd }) => {
         }
         
     }
+    const handleSubmit = (event) => {
+        const inputValue = event.target.elements.search.value;
+        event.preventDefault();
+        if (inputValue === "") {
+            toast.error('Please, write something!');
+            
+        } else {
+            
+            onAdd(inputValue);
+        }
+        
+    }
 
 
     return (
