1c606b81f16e41c278f757120d73eb2b10b1c30f
VovaDev777 <polvovater@gmail.com>
2024-07-15 23:57:43 +0200

    Update ImageCard.jsx

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -12,32 +12,32 @@
 
 // export default ImageCard
 
-// import { useState } from 'react';
-// import Modal from 'react-modal';
-// import css from './ImageCard.module.css';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import css from './ImageCard.module.css';
 
-// Modal.setAppElement('#root');
+Modal.setAppElement('#root');
 
-// const ImageCard = ({ url, desc, modalUrl }) => {
-//   const [modalIsOpen, setModalIsOpen] = useState(false);
+const ImageCard = ({ url, desc, modalUrl }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-//   const openModal = () => setModalIsOpen(true);
-//   const closeModal = () => setModalIsOpen(false);
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
 
-//   return (
-//     <div>
-//       <img src={url} alt={desc} className={css.image} onClick={openModal} />
-//       <Modal
-//         isOpen={modalIsOpen}
-//         onRequestClose={closeModal}
-//         contentLabel="Image Modal"
-//         className={css.modalContent}
-//         overlayClassName={css.modalOverlay}
-//       >
-//         <img src={modalUrl} alt={desc} className={css.modalImage} />
-//       </Modal>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <img src={url} alt={desc} className={css.image} onClick={openModal} />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+        className={css.modalContent}
+        overlayClassName={css.modalOverlay}
+      >
+        <img src={modalUrl} alt={desc} className={css.modalImage} />
+      </Modal>
+    </div>
+  );
+}
 
-// export default ImageCard;
+export default ImageCard;
