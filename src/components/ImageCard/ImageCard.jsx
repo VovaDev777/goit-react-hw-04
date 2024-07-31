8edf31168a12f354d226fbad9a6dfeee522f8cef
VovaDev777 <polvovater@gmail.com>
2024-07-31 16:54:41 +0200

    1

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,43 +1,42 @@
-// import css from './ImageCard.module.css'
-// import Modal from 'react-modal';
+import css from './ImageCard.module.css'
 
 
-// const ImageCard = ({url, desc}) => {
-//   return (
-//     <div>
-//       <img src={url} alt={desc} className={css.image}/>
-//     </div>
-//   )
-// }
-
-// export default ImageCard
-
-import { useState } from 'react';
-import Modal from 'react-modal';
-import css from './ImageCard.module.css';
-
-Modal.setAppElement('#root');
-
-const ImageCard = ({ url, desc, modalUrl }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => setModalIsOpen(true);
-  const closeModal = () => setModalIsOpen(false);
-
+const ImageCard = ({url, desc}) => {
   return (
     <div>
-      <img src={url} alt={desc} className={css.image} onClick={openModal} />
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Image Modal"
-        className={css.modalContent}
-        overlayClassName={css.modalOverlay}
-      >
-        <img src={modalUrl} alt={desc} className={css.modalImage} />
-      </Modal>
+      <img src={url} alt={desc} className={css.image}/>
     </div>
-  );
+  )
 }
 
-export default ImageCard;
+export default ImageCard
+
+// import { useState } from 'react';
+// import Modal from 'react-modal';
+// import css from './ImageCard.module.css';
+
+// Modal.setAppElement('#root');
+
+// const ImageCard = ({ url, desc, modalUrl }) => {
+//   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+//   const openModal = () => setModalIsOpen(true);
+//   const closeModal = () => setModalIsOpen(false);
+
+//   return (
+//     <div>
+//       <img src={url} alt={desc} className={css.image} onClick={openModal} />
+//       <Modal
+//         isOpen={modalIsOpen}
+//         onRequestClose={closeModal}
+//         contentLabel="Image Modal"
+//         className={css.modalContent}
+//         overlayClassName={css.modalOverlay}
+//       >
+//         <img src={modalUrl} alt={desc} className={css.modalImage} />
+//       </Modal>
+//     </div>
+//   );
+// }
+
+// export default ImageCard;
